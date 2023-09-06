7567637c1e2b79a80fb18333402fe92d036b0866
Mohit sahu@DESKTOP-NSE9EHD <sahumohit643@gmail.com>
2023-09-06 11:30:50 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Routes,Route } from 'react-router';
 import Home from './Components/Home/Home';
 import About from  './Components/About/About';
 import Navbar from './Components/Navbar/Navbar';
+import ContactUs from './Components/Contact us/ContactUs';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
       <Routes>
-        <Route path='/' element={<Navbar/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/contact us' element={<ContactUs/>}/>
       </Routes>
+      
+      
     </div>
   );
 }
